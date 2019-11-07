db1d8e8b1fae99fffbf72951122ea3d205e1d694
katolikov <55280831+katolikov@users.noreply.github.com>
2019-11-07 23:35:56 +0300

    Add files via upload

--- client_new.c
+++ client_new.c
@@ -17,7 +17,7 @@ int main(){
      FILE *g;
      struct shmid_ds buf;
 
-     printf("\n\033[1;35m[INFO]\033[0m Client working.\n");
+     printf("\n\033[1;35m[INFO]\033[0m \033[5mClient working.\033[0m\n");
      /* Получим доступ к разделяемой памяти */
      if ((shmid = shmget(key, sizeof(Message), 0)) < 0) {
          perror("\033[1;31m[ERROR] shmget.");
@@ -31,26 +31,23 @@ int main(){
          return 1;
      }
      /* Получим доступ к семафору */
-     if ((semid = semget(key, 2, PERM)) < 0) {
+     if ((semid = semget(key, 1, PERM)) < 0) {
          perror("\033[1;31m[ERROR] semget.");
          printf("\033[0m");
          return 1;
      }
-     /* Заблокируем разделяемую память */
-     if (semop(semid, &mem_lock[0], 2) < 0) {
+     /* разаблокируем разделяемую память */
+      if (semop(semid,unlock,1) < 0) {
          perror("\033[1;31m[ERROR] Block memory error.");
          printf("\033[0m");
          return 1;
      }
-
-     /* Уведомим сервер о начале работы */
-     if (semop(semid, &proc_start[0], 1) < 0) {
-         perror("\033[1;31m[ERROR] Server don't understand.");
-         printf("\033[0m");
-         return 1;
-     }
-
-     /* Освободим разделяемую память */
+     // C помощью этого системного вызова можно запрашивать и изменять управляющие параметры набора семафоров с дескриптором semfd (1),
+     // а также удалять семафор. Значение num (2) — это индекс семафора, cmd (3)— задает операцию,
+     // которая должна быть выполнена над конкретным семафором данного набора:
+     // (копировать управляющие параметры набора семафоров в объект, указанный аргументом arg.buf.)
+     // arg (4) — это объект типа union semun, который может использоваться для задания или
+     // выборки управляющих параметров набора семафоров в соответствии с аргументом cmd.
      if(shmctl(shmid, IPC_STAT, &buf) < 0){
          perror("\033[1;31m[ERROR] shmctl.");
          printf("\033[0m");
@@ -64,11 +61,11 @@ int main(){
          printf("\033[0m");
          return 1;
      }
-     if (semop(semid, &mem_unlock[0], 1) < 0){
+     /*if (semop(semid, &mem_unlock[0], 1) < 0){
          perror("\033[1;31m[ERROR] Unlock memory error.");
          printf("\033[0m");
          return 1;
-     }
+     }*/
 
      if(access("haram", F_OK)==-1){
 
@@ -81,7 +78,7 @@ int main(){
           system("rm haram");
           close(fd);
 
-      }
+    }
     else printf("\033[1;35m[INFO] Rename your file: haram\n");
 
     printf("\n\033[1;35m[INFO]\033[0m PROCESS STATE CODES\n\nD \033[1;35muninterruptible sleep (usually IO)\n\033[0mR \033[1;35mrunning or runnable (on run queue)\n\033[0mS \033[1;35minterruptible sleep (waiting for an event to complete)\n\033[0mT \033[1;35mstopped, either by a job control signal or because it is being traced.\n\033[0mW \033[1;35mpaging (not valid since the 2.6.xx kernel)\n\033[0mX \033[1;35mdead (should never be seen)\n\033[0mZ \033[1;35mdefunct ('zombie') process, terminate but not reaped by its parent.\n\nFor \033[0mBSD\033[1;35m formats and when the stat keyword is used, additional characters may\n\033[0m<\033[1;35m high-priority (not nice to other users)\n\033[0mN\033[1;35m low-priority (nice to other users)\n\033[0mL\033[1;35m has pages locked into memory (for real-time and custom IO)\n\033[0ms\033[1;35m is a session leader\n\033[0ml\033[1;35m is multi-threaded (using CLONE_THREAD, like NPTL pthreads do)\n\033[0m+\033[1;35m is in the foreground process group.\n\n");
